725c240a73dc23b84f280d8656612fea7bef7e52
bukun678 <sherfycedrickzpcp2056@gmail.com>
2025-08-17 22:21:27 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -185,7 +185,7 @@ export default function TryOnNails() {
           filesetResolver,
           {
             baseOptions: {
-              modelAssetPath: "/hand_landmarker.task",
+              modelAssetPath: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
             },
             numHands: 1,
             runningMode: "IMAGE",
